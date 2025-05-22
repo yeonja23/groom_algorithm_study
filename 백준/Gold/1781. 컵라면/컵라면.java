b37dfbe38e9a669178b0fd0e6b020b2d3c3b34b3
yeonja23 <163745971+yeonja23@users.noreply.github.com>
2025-05-22 14:35:46 +0900

    [Gold II] Title: 컵라면, Time: 724 ms, Memory: 84720 KB -BaekjoonHub

---
+++ 백준/Gold/1781. 컵라면/컵라면.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<int[]> problems = new PriorityQueue<>((a, b)->{
+            int result = a[0]-b[0];
+            return result == 0 ? b[1] - a[1] : result;
+        });
+
+        while (N-- > 0) {
+            String[] input = br.readLine().split(" ");
+            int deadLine = Integer.parseInt(input[0]);
+            int numOfRamen = Integer.parseInt(input[1]);
+
+            problems.offer(new int[]{deadLine, numOfRamen});
+        }
+
+        PriorityQueue<Integer> select = new PriorityQueue<>();
+
+        while (!problems.isEmpty()) {
+            int[] problem = problems.poll();
+            int deadLine = problem[0];
+            int numOfRamen = problem[1];
+
+            if (select.size() < deadLine) {
+                select.offer(numOfRamen);
+            } else {
+                if (select.peek() < numOfRamen) {
+                    select.poll();
+                    select.offer(numOfRamen);
+                }
+            }
+        }
+
+        long result = 0L;
+        for (Integer numOfRamen : select) {
+            result += numOfRamen;
+        }
+        System.out.println(result);
+    }
+}
